4b8bca8455083a84f5c6790554bc430d8e4b1e92
Jiaming Luo <j_luo@rosetta3.csail.mit.edu>
2018-04-03 13:09:13 -0400

    fix tracking in log_uniform

---
+++ log_uniform/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+setup(ext_modules = cythonize(Extension(
+           "log_uniform",                                # the extension name
+           sources=["log_uniform.pyx", "Log_Uniform_Sampler.cpp"], # the Cython source and additional C++ source files
+           language="c++",                        # generate and compile C++ code
+           extra_compile_args=["-std=c++11"],
+           extra_link_args=['-L/usr/lib/x86_64-linux-gnu/']
+      )))
